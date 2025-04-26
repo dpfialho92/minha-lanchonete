f6521967fdc812a2d17a0a0bc8a0c4d987015d2c
dpfialho92 <dani.boop22@gmail.com>
2025-04-26 07:26:11 -0300

    teste

--- src/App.jsx
+++ src/App.jsx
@@ -59,7 +59,7 @@ function App() {
 
         <div class="titulo-Cardapio">
           <a><h1>LANCHE+</h1></a>
-          <img src="/public/images/burger.png" alt="Logo da Lanchonete" className="logo-img" />
+          <img src="/public/images/burguer.png" alt="Logo da Lanchonete" className="logo-img" />
         </div>
 
 
